b65dbefb4a6287a8b8e31823e3850bb8f8ff7758
Zein <zeinkasar123@gmail.com>
2023-01-02 14:52:21 +0100

    Auth dependencies toegevoegd
    Auth dependencies toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Upload from "./pages/Upload";
+import Post from "./pages/Post";
+import Profile from "./pages/Profile";
+import AppNavbar from "./components/AppNavbar";
+import { CookiesProvider } from 'react-cookie';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CookiesProvider>
+        <AppNavbar />
+      </CookiesProvider>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/upload" element={<Upload />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
